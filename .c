0f90200353d1212135b48699db32650f0638fae3
Vladimirlopez9 <152047123+Vladimirlopez9@users.noreply.github.com>
2023-11-25 21:55:42 -0600

    Create .c

---
+++ .c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include "proyecto final modde.h"
+
+struct Tarea tareas[MAX_TAREAS];
+int cantidadTareas = 0;
+
+void mostrarMenu() {
+    printf("\n1. Agregar una nueva tarea.\n");
+    printf("2. Marcar una tarea como completada.\n");
+    printf("3. Listar todas las tareas pendientes.\n");
+    printf("4. Listar todas las tareas.\n");
+    printf("5. Buscar una tarea por titulo.\n");
+    printf("6. Salir.\n");
+}
+
+void agregarTarea() {
+    if (cantidadTareas < MAX_TAREAS) {
+        printf("Ingrese el titulo de la tarea: ");
+        scanf("%s", tareas[cantidadTareas].titulo);
+        tareas[cantidadTareas].completada = 0; 
+        cantidadTareas++;
+        printf("Tarea agregada con exito.\n");
+    } else {
+        printf("Se ha alcanzado el limite maximo de tareas.\n");
+    }
+}
+
+void marcarTareaCompletada() {
+    int indiceTarea;
+    printf("Ingrese el numero de la tarea que desea marcar como completada: ");
+    scanf("%d", &indiceTarea);
+    if (indiceTarea >= 0 && indiceTarea < cantidadTareas) {
+        tareas[indiceTarea].completada = 1;
+        printf("Tarea marcada como completada.\n");
+    } else {
+        printf("Indice de tarea no valido.\n");
+    }
+}
+
+void listarTareasPendientes() {\
+    int i;
+    printf("Tareas pendientes:\n");
+    for ( i = 0; i < cantidadTareas; i++) {
+        if (tareas[i].completada == 0) {
+            printf("%d. %s (pendiente)\n", i, tareas[i].titulo);
+        }
+    }
+}
+
+void listarTodasTareas() {
+	int i;
+    printf("Todas las tareas:\n");
+    for (i = 0; i < cantidadTareas; i++) {
+        printf("%d. %s (%s)\n", i, tareas[i].titulo, tareas[i].completada == 0 ? "pendiente" : "completada");
+    }
+}
+
+void buscarTarea(char titulo[]) {
+    int encontrada = 0,i;
+    printf("Resultados de la busqueda:\n");
+    for (i = 0; i < cantidadTareas; i++) {
+        if (strcmp(tareas[i].titulo, titulo) == 0) {
+            printf("%d. %s (%s)\n", i, tareas[i].titulo, tareas[i].completada == 0 ? "pendiente" : "completada");
+            encontrada = 1;
+        }
+    }
+    if (!encontrada) {
+        printf("La tarea \"%s\" no se encontro en la lista.\n", titulo);
+    }
+}
