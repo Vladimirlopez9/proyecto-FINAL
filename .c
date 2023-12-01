b39123545da09451599b4b6648e21b5452048a9f
Vladimirlopez9 <152047123+Vladimirlopez9@users.noreply.github.com>
2023-12-01 00:39:30 -0600

    Update .c

--- .c
+++ .c
@@ -56,16 +56,21 @@ void listarTodasTareas() {
     }
 }
 
-void buscarTarea(char titulo[]) {
-    int encontrada = 0,i;
-    printf("Resultados de la busqueda:\n");
-    for (i = 0; i < cantidadTareas; i++) {
-        if (strcmp(tareas[i].titulo, titulo) == 0) {
-            printf("%d. %s (%s)\n", i, tareas[i].titulo, tareas[i].completada == 0 ? "pendiente" : "completada");
-            encontrada = 1;
-        }
-    }
-    if (!encontrada) {
+void buscarTareaRecursiva(char titulo[], int index) {
+    if (index >= cantidadTareas) {
         printf("La tarea \"%s\" no se encontro en la lista.\n", titulo);
+        return;
     }
+
+    if (strcmp(tareas[index].titulo, titulo) == 0) {
+        printf("%d. %s (%s)\n", index, tareas[index].titulo, tareas[index].completada == 0 ? "pendiente" : "completada");
+        return;
+    }
+
+    buscarTareaRecursiva(titulo, index + 1);
+}
+
+void buscarTarea(char titulo[]) {
+    printf("Resultados de la busqueda:\n");
+    buscarTareaRecursiva(titulo, 0);
 }
